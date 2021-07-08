7cbd3a0d36ebc22bf466f95ddb23f771e52c2741
Ngoni Mapfumo <ngonimapfumo@hotmail.com>
2021-07-08 21:33:18 +0200

    map

--- app/src/main/java/com/ngoni/arcgis/MainActivity.java
+++ app/src/main/java/com/ngoni/arcgis/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
 import com.esri.arcgisruntime.ArcGISRuntimeEnvironment;
+import com.esri.arcgisruntime.layers.ArcGISMapImageLayer;
 import com.esri.arcgisruntime.mapping.ArcGISMap;
 import com.esri.arcgisruntime.mapping.BasemapStyle;
 import com.esri.arcgisruntime.mapping.Viewpoint;
@@ -25,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void setUpMap() {
         ArcGISRuntimeEnvironment.setApiKey(BuildConfig.API_KEY);
-        ArcGISMap map = new ArcGISMap(BasemapStyle.ARCGIS_COLORED_PENCIL);
+        ArcGISMap map = new ArcGISMap(BasemapStyle.ARCGIS_MIDCENTURY);
         mMapView.setMap(map);
         mMapView.setViewpoint(new Viewpoint(-17.824858, 31.053028, 72000.0));
         /*-17.8577368116804, 31.038344197312732*/
