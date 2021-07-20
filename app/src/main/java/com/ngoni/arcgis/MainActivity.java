b82c33330990142a393e8f7f7e179a386824acca
ngoni <ngonimapfumo@hotmail.com>
2021-07-21 01:33:22 +0200

    Location

--- app/src/main/java/com/ngoni/arcgis/MainActivity.java
+++ app/src/main/java/com/ngoni/arcgis/MainActivity.java
@@ -1,12 +1,14 @@
 package com.ngoni.arcgis;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.esri.arcgisruntime.ArcGISRuntimeEnvironment;
-import com.esri.arcgisruntime.layers.ArcGISMapImageLayer;
 import com.esri.arcgisruntime.mapping.ArcGISMap;
 import com.esri.arcgisruntime.mapping.BasemapStyle;
 import com.esri.arcgisruntime.mapping.Viewpoint;
@@ -15,20 +17,24 @@ import com.esri.arcgisruntime.mapping.view.MapView;
 public class MainActivity extends AppCompatActivity {
 
     private MapView mMapView;
+    Location mLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mMapView = findViewById(R.id.arc_map);
+        mLocation = new Location(MainActivity.this);
         setUpMap();
+        checkPermissions();
     }
 
     private void setUpMap() {
         ArcGISRuntimeEnvironment.setApiKey(BuildConfig.API_KEY);
-        ArcGISMap map = new ArcGISMap(BasemapStyle.ARCGIS_MIDCENTURY);
+        ArcGISMap map = new ArcGISMap(BasemapStyle.ARCGIS_COMMUNITY);
         mMapView.setMap(map);
-        mMapView.setViewpoint(new Viewpoint(-17.824858, 31.053028, 72000.0));
+        mMapView.setViewpoint(new Viewpoint(mLocation.getLatitude(), mLocation.getLongitude(), 72000.0));
+        Toast.makeText(this, mLocation.latitude+"", Toast.LENGTH_SHORT).show();
         /*-17.8577368116804, 31.038344197312732*/
 
     }
@@ -50,4 +56,14 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
         mMapView.dispose();
     }
+    private void checkPermissions(){
+        try {
+            if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ) {
+                ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 101);
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
 }
